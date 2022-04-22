d7ed126337c8a8b1bf16dca357dc0ed89285546a
Aditya <aditya21official@gmail.com>
2022-04-22 17:35:52 +0530

    Blog feature updated

--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,21 @@
+import { useState } from "react";
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log('helo, ninjas', e)
-    }
-    
+    const [blogs, setBlogs] = useState([
+        { title: 'My ew website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 },
+    ]);
 
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click me</button>
-            
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{blog.title}</h2>
+                    <p>Written byt { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }

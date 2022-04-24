7888cc00c27717e008c6e57e995cd641591960f5
Aditya <aditya21official@gmail.com>
2022-04-24 18:48:35 +0530

    React Router Updated

--- src/Home.js
+++ src/Home.js
@@ -1,42 +1,17 @@
-import { useState, useEffect } from "react";
+// import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
-
-
-    useEffect(() => {
-        setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-                .then(res => {
-                    if (!res.ok) {
-                        throw Error('could not fetch data for that resource');
-                    }
-                    return res.json();
-                })
-                .then(data => {
-                    setBlogs(data)
-                    setIsPending(false);
-                    setError(null);
-                })
-            .catch(err => {
-                setIsPending(false);
-                setError(err.message);
-            })    
-
-        }, 1000)
-    }, []);
-
-    return ( 
-        <div className="home">
-            { error && <div>{ error }</div> }
-            { isPending && <div>Loading...</div> }
-            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}   
-        </div>
-     );
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
 }
  
 export default Home;
